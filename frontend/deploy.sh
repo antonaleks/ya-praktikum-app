49cc4ea1db3a2d99a38ab607ff0a33e82cef6189
alekseev <alekseev.ap605@gmail.com>
2022-06-05 15:13:36 +0300

    front env

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,6 +7,7 @@ sudo rm -rf /home/front-user/dist||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:$(echo $NEXUS_REPO_PASS | base64 -d) -o sausage-store.tar.gz ${NEXUS_REPO_FRONTEND_URL}/sausage-store-front/sausage-store/$VERSION/sausage-store-$VERSION.tar.gz
 sudo tar -xf ./sausage-store.tar.gz --strip-components 2 -C /home/front-user/front||true
+echo "FRONT_PORT=$FRONT_PORT" > /etc/default/sausage-store-front
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
