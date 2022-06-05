7e11c6638b6ec74ae4774d4cd1d45a6780d61f11
Антон Алексеев <gorlym2699@yandex.ru>
2022-06-05 10:06:08 +0000

    Masked variables

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,7 +5,7 @@ set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -rf /home/front-user/dist||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_FRONTEND_URL}/sausage-store-front/sausage-store/$VERSION/sausage-store-$VERSION.tar.gz
+curl -u ${NEXUS_REPO_USER}:$(echo $NEXUS_REPO_PASS | base64 -d) -o sausage-store.tar.gz ${NEXUS_REPO_FRONTEND_URL}/sausage-store-front/sausage-store/$VERSION/sausage-store-$VERSION.tar.gz
 sudo tar -xf ./sausage-store.tar.gz --strip-components 2 -C /home/front-user/front||true
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
