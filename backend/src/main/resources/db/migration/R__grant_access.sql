f75ccaaa103939cdd974d87d0637870f018c9bc4
alekseev <alekseev.ap605@gmail.com>
2022-07-02 22:53:13 +0300

    flyway migration fix grant access

--- backend/src/main/resources/db/migration/R__grant_access.sql
+++ backend/src/main/resources/db/migration/R__grant_access.sql
@@ -1,3 +1,3 @@
-GRANT SELECT ON DATABASE "sausage-store-db" TO read_user;
-GRANT SELECT, INSERT, UPDATE, DELETE ON DATABASE "sausage-store-db" TO write_user;
-GRANT ALL PRIVILEGES ON DATABASE "sausage-store-db" TO admin_user;
+GRANT SELECT ON sausage-store-db TO read_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON sausage-store-db TO write_user;
+GRANT ALL PRIVILEGES ON sausage-store-db TO admin_user;
